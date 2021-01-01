85e357d69350fe72fa6cb719fa8051cda916ea3f
jhnsartain3 <jhnsartain3@gmail.com>
2020-12-31 19:19:13 -0600

    add Cryptography

---
+++ Sartain Studios Common Tests/Hasher/HasherTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Sartain_Studios_Common_Tests.Hasher
+{
+    public class HasherTests
+    {
+        private Sartain_Studios_Common.Cryptography.Hasher _hasher;
+
+        [SetUp]
+        public void Setup()
+        {
+            _hasher = new Sartain_Studios_Common.Cryptography.Hasher();
+        }
+
+        [Test]
+        public void GenerateHash_CreatesStringOf128Characters()
+        {
+            Assert.AreEqual(128, _hasher.GenerateHash("fdsaf").Length);
+        }
+    }
+}
